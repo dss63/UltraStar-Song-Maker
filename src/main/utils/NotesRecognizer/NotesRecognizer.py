f331bd902d7b2cb1775ed495cb5d8857ce3a03f6
dss63 <dss63@gcloud.ua.es>
2023-03-07 01:31:20 +0100

    .

--- src/main/utils/NotesRecognizer/NotesRecognizer.py
+++ src/main/utils/NotesRecognizer/NotesRecognizer.py
@@ -1,26 +1,84 @@
 import numpy as np
 import librosa
+# from librosa.core import hz_to_noteName
 from pathlib import Path
+# import music21
+# from pydub import AudioSegment
+# from pydub.playback import play
 
+def recognizeNotes():
 
-def recognizeNotes(file):
+    # extractVoice(file)
 
-    extractVoice(file)
-
-    filepath = Path("/home/aureortiz/Projects/USSM/src/resources/songs/personalidad/output/song/vocals.wav")
+    filepath = Path("C:/Users/danie/Desktop/UltraStar-Song-Maker/src/main/utils/NotesRecognizer/vocals.wav")
 
     y, sr = librosa.load(filepath)
     print(y)
     print(sr)
-    pitches, magnitudes = librosa.piptrack(y, sr=sr)
-    notes = []
-    for i in range(pitches.shape[0]):
-        curr_pitch = pitches[i]
-        curr_magnitude = magnitudes[i].max()
-        note = librosa.hz_to_note(curr_pitch, octave=False, cents=True)
-        if curr_magnitude > 0.5:
-            notes.append(note)
-    return notes
 
-def extractVoice(file):
-    return 0
+    # Prueba que esta bien cargada la canción
+    # with open("notas.txt", "w") as f:
+    #     for i in range(len(y)):
+    #         f.write(str(y[i])+"\n")
+
+    # # Extraer las frecuencias y la magnitud de la señal de audio
+    # freqs, magnitudes = librosa.core.magphase(librosa.stft(y))
+
+    # # Convertir las magnitudes a dB
+    # magnitudes_db = librosa.amplitude_to_db(magnitudes)
+
+    # # Encontrar las frecuencias dominantes en cada marco de tiempo
+    # frequencies = librosa.core.fft_frequencies(sr=sr, n_fft=2048)
+    # times = librosa.core.frames_to_time(np.arange(magnitudes_db.shape[1]), sr=sr, hop_length=512)
+    # freqs_idx = np.argmax(magnitudes_db, axis=0)
+    # dominant_freqs = frequencies[freqs_idx]
+
+    # # Asignar las frecuencias a notas musicales
+    # note_names = librosa.hz_to_note(dominant_freqs)
+
+    # # Escribir las notas en un archivo de texto
+    # with open('notas.txt', 'w') as f:
+    #     for i in range(len(note_names)):
+    #         f.write("{:.2f} {}\n".format(times[i], note_names[i]))
+
+
+    # # Leer archivo mp3
+    # song = AudioSegment.from_mp3(filepath)
+
+    # # Extraer notas musicales
+    # samples = song.get_array_of_samples()
+
+    # # Convertir a objeto numpy
+    # notes = np.array(samples)
+
+
+    # # Escribir notas en archivo txt
+    # with open("notas.txt", "w") as f:
+    #     f.write(str(notes))
+
+    # y, sr = librosa.load(filepath)
+    # print(len(y))
+    # print("sr",sr)
+    # pitches, magnitudes = librosa.piptrack(y, sr=sr)
+    # notes = []
+    # for i in range(pitches.shape[0]):
+    #     curr_pitch = pitches[i]
+    #     curr_magnitude = magnitudes[i].max()
+    #     note = librosa.hz_to_note(curr_pitch, octave=False, cents=True)
+    #     if curr_magnitude > 0.5:
+    #         notes.append(note)
+    # return notes
+
+    # Convertir audio a notas musicales
+#     stream = music21.converter.subConverters.ConverterMEI(filepath)
+#     notes = stream.flat.notes
+#     output_file = 'notas.txt'
+#     with open(output_file, 'w') as f:
+#         for note in notes:
+#             f.write(str(note.pitch) + ' ' + str(note.offsetSeconds) + '\n')
+
+# def extractVoice(file):
+#     return 0
+
+
+recognizeNotes()
--- src/main/utils/Reader/FileReader.py
+++ src/main/utils/Reader/FileReader.py
@@ -6,3 +6,102 @@ class FileReader:
     def readFile(self, path):
         f = open(path, "r")
         return f.readlines()
+
+        
+from tkinter import *
+
+def submit_form():
+    title = title_entry.get()
+    artist = artist_entry.get()
+    language = language_entry.get()
+    genre = genre_entry.get()
+    year = year_entry.get()
+    image = image_entry.get()
+    video = video_entry.get()
+
+    # Guardar los datos en un archivo de texto
+    with open("datos.txt", "w") as f:
+        f.write("#TITLE:" + title + "\n")
+        f.write("#ARTIST:" + artist + "\n")
+        f.write("#LANGUAGE:" + language + "\n")
+        f.write("#GENRE:" + genre + "\n")
+        f.write("#YEAR:" + year + "\n")
+        f.write("#CREATOR:Daniel Santoyo \n")
+
+        f.write("#MP3: --- PONER EL NOMBRE DEL ARCHIVO DE LA CANCION ---\n")
+
+        f.write("#COVER:" + image + "\n")
+        f.write("#VIDEO:" + video + "\n")
+
+        f.write("#VIDEOGAP: --- PONER VIDEOGAP --- \n")
+        f.write("#BPM: --- PON EL BEAT POR MINUTO DE LA CANCION --- \n")
+        f.write("#GAP: --- PON EL GAP DE LA CANCION --- \n")
+
+        #START
+        #END
+        #VIDEOGAP
+        #BPM
+        #GAP
+        #MEDLEYSTARTBEAT
+        #MEDLEYENDBEAT
+
+        #NOTAS Y DE MAS MOVIDAS
+
+# Crear ventana principal
+root = Tk()
+
+# Crear campos de entrada para el formulario
+title_label = Label(root, text="Título:")
+title_entry = Entry(root)
+
+artist_label = Label(root, text="Artista:")
+artist_entry = Entry(root)
+
+language_label = Label(root, text="Lenguaje:")
+language_entry = Entry(root)
+
+genre_label = Label(root, text="Género:")
+genre_entry = Entry(root)
+
+year_label = Label(root, text="Año:")
+year_entry = Entry(root)
+
+image_label = Label(root, text="Imagen:")
+image_entry = Entry(root)
+
+video_label = Label(root, text="Video:")
+video_entry = Entry(root)
+
+# Crear botón para enviar el formulario
+submit_button = Button(root, text="Enviar", command=submit_form)
+
+# Acomodar los campos en la ventana
+title_label.grid(row=0, column=0)
+title_entry.grid(row=0, column=1)
+
+artist_label.grid(row=1, column=0)
+artist_entry.grid(row=1, column=1)
+
+language_label.grid(row=2, column=0)
+language_entry.grid(row=2, column=1)
+
+genre_label.grid(row=3, column=0)
+genre_entry.grid(row=3, column=1)
+
+year_label.grid(row=4, column=0)
+year_entry.grid(row=4, column=1)
+
+image_label.grid(row=5, column=0)
+image_entry.grid(row=5, column=1)
+
+video_label.grid(row=6, column=0)
+video_entry.grid(row=6, column=1)
+
+submit_button.grid(row=7, column=1)
+
+submit_form()
+
+# Ejecutar la ventana principal
+root.mainloop()
+
+
