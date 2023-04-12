50a54f8debb7b79e96c817c551e55c58bf802de8
dss63 <dss63@gcloud.ua.es>
2023-04-13 00:34:53 +0200

    acapella

--- src/main/Application.py
+++ src/main/Application.py
@@ -12,24 +12,17 @@ from pathlib import Path
 
 if __name__ == '__main__':
 
-    #Instancia de la clase
-
-    #Crear el fichero y abrir formulario de información que debe otorgar el usuario
-
-    #Crear audio vocal e instrumental
-
-    #Enviar ambos audios a la clase de reconocer notas donde acaba el proceso completo
-
     # Instancia de la clase
     NT = NotesRecognizer.NotesUtils()
     NA = Acapella
 
     #Crear audio vocal e instrumental
-    # NA.separarAudio("src/main/utils/NotesRecognizer/alarma2.mp3","src/main/utils/NotesRecognizer/")
+    NA.separarAudio("src/main/utils/NotesRecognizer/alarma2.mp3","src/main/utils/NotesRecognizer/")
 
     # # Reconocer notas y guardarlas en fichero
-    NT.reconocerNotas("src/main/utils/NotesRecognizer/alarma.mp3")
-    NT. reconocerTempo("src/main/utils/NotesRecognizer/alarma2.mp3")
+    NT.reconocerNotas("src/main/utils/NotesRecognizer/solovoz.wav")
+    NT. reconocerTempo("src/main/utils/NotesRecognizer/solomusica.wav")
+
     # Leo el fichero que contiene las frecuencias
     freqList = NT.leerFichero()
 
--- src/main/utils/NotesRecognizer/Acapella.py
+++ src/main/utils/NotesRecognizer/Acapella.py
@@ -1,57 +1,21 @@
-import wave
-import numpy as np
-from scipy.signal import butter, lfilter
+import os
+from spleeter.separator import Separator
 
 
 def separarAudio(input_file, output_dir):
 
-      # Frecuencia de muestreo
-   Fs = 44100
+   # Load the pre-trained spleeter model
+   separator = Separator('spleeter:2stems')
 
-   # Frecuencia de corte para el filtro pasa-banda
-   lowcut = 300
-   highcut = 3000
+   # Separate the vocals and instrumental from the input audio file
+   separator.separate_to_file(input_file, output_dir, codec='wav')
 
-   # Orden del filtro
-   order = 2
 
-   # Coeficientes del filtro
-   def butter_bandpass(lowcut, highcut, fs, order=5):
-      nyq = 0.5 * fs
-      low = lowcut / nyq
-      high = highcut / nyq
-      b, a = butter(order, [low, high], btype='band')
-      return b, a
+   # Mover archivo vocal.wav al directorio "vocals"
+   os.rename(os.path.join(output_dir+"alarma2", "vocals.wav"), os.path.join(output_dir, "solovoz.wav"))
+   os.rename(os.path.join(output_dir+"alarma2", "accompaniment.wav"), os.path.join(output_dir, "solomusica.wav"))
 
-   # Lectura del archivo de audio
-   audio = wave.open("src/main/utils/NotesRecognizer/lion.wav", 'rb')
-
-   # Obtención de los datos del audio
-   nframes = audio.getnframes()
-   data = audio.readframes(nframes)
-
-   # Conversión de los datos del audio a un array numpy
-   audio_array = np.frombuffer(data, dtype=np.int16)
-
-   # Aplicación del filtro pasa-banda para separar la voz de la música
-   b, a = butter_bandpass(lowcut, highcut, Fs, order=order)
-   voice_array = lfilter(b, a, audio_array)
-
-   # Creación de los archivos de salida
-   voice_wave = wave.open('src/main/utils/NotesRecognizer/aaaaaaa.wav', 'w')
-   voice_wave.setnchannels(1)
-   voice_wave.setsampwidth(audio.getsampwidth())
-   voice_wave.setframerate(audio.getframerate())
-   voice_wave.writeframes(voice_array)
-
-   music_array = audio_array - voice_array
-   music_wave = wave.open('src/main/utils/NotesRecognizer/aaaaaae.wav', 'w')
-   music_wave.setnchannels(1)
-   music_wave.setsampwidth(audio.getsampwidth())
-   music_wave.setframerate(audio.getframerate())
-   music_wave.writeframes(music_array)
-
-   # Cierre de los archivos
-   audio.close()
-   voice_wave.close()
-   music_wave.close()
+   try:
+      os.rmdir(output_dir+"alarma2")
+   except OSError as error:
+      print(f"Ocurrió un error al eliminar la carpeta: {error}")
